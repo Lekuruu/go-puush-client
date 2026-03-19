0b8269db2b81c7d9554dd629d11e8a6ee6fa3d60
Levi <contact@lekuru.xyz>
2026-03-19 18:07:37 +0100

    Initialize config on application startup & add reference to ui struct

--- cmd/desktop/main.go
+++ cmd/desktop/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 
+	"github.com/Lekuruu/go-puush-client/internal/config"
 	"github.com/Lekuruu/go-puush-client/internal/desktop"
 )
 
 func main() {
 	app := app.NewWithID("me.puush.client")
-	ui := desktop.NewUI(app)
+
+	store := config.NewStore()
+	cfg, err := store.Load()
+	if err != nil {
+		cfg = config.DefaultConfig()
+	}
+
+	ui := desktop.NewUI(app, cfg)
 	ui.Run()
 }
--- internal/desktop/app.go
+++ internal/desktop/app.go
@@ -2,16 +2,19 @@ package desktop
 
 import (
 	"fyne.io/fyne/v2"
+
+	"github.com/Lekuruu/go-puush-client/internal/config"
 )
 
 // UI manages the desktop application windows and state.
 type UI struct {
-	app fyne.App
+	app    fyne.App
+	config *config.Config
 }
 
-func NewUI(app fyne.App) *UI {
+func NewUI(app fyne.App, cfg *config.Config) *UI {
 	app.Settings().SetTheme(NewWindowsTheme())
-	return &UI{app: app}
+	return &UI{app: app, config: cfg}
 }
 
 func (ui *UI) Run() {

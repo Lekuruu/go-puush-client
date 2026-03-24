7d2a531bed313e45826a0ad7020c8b21acfa8c6e
Levi <contact@lekuru.xyz>
2026-03-24 19:52:35 +0100

    Add todo for file system watcher

--- internal/tray/watch.go
+++ internal/tray/watch.go
@@ -1,1 +1,4 @@
 package tray
+
+// TODO: Implement file system watcher to detect new uploads in a folder.
+// 		 This functionality was not present in the original puush client, but it would be an awesome feature.

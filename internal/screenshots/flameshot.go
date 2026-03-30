3ca3af4a4d551e27bd8fbe718c14c5f3f5e821c0
Levi <contact@lekuru.xyz>
2026-03-30 17:35:56 +0200

    Add todo's for more linux screenshot tools

---
+++ internal/screenshots/flameshot.go
@@ -0,0 +1,5 @@
+//go:build linux
+
+package screenshots
+
+// TODO: Implement flameshot screenshot provider
---
+++ internal/screenshots/gnome.go
@@ -0,0 +1,5 @@
+//go:build linux
+
+package screenshots
+
+// TODO: Implement gnome screenshot provider

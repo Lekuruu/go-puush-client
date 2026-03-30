5305abe6b9c58d5dae86375ac016878fdcfd4d94
Levi <contact@lekuru.xyz>
2026-03-30 18:30:58 +0200

    Implement clipboard uploader

--- internal/tray/upload_clipboard.go
+++ internal/tray/upload_clipboard.go
@@ -1,6 +1,24 @@
 package tray
 
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"fyne.io/fyne/v2"
+)
+
 func (m *TrayManager) UploadFromClipboard() {
-	// TODO: Implement clipboard system
-	//       This will most likely require more platform specific code... yay.
+	// NOTE: This should work in theory, but there's probably some wayland shenanigans that doens't allow it idk
+	content := fyne.CurrentApp().Clipboard().Content()
+
+	if content == "" {
+		m.ShowErrorNotification("Your clipboard is empty or does not contain any text.")
+		return
+	}
+
+	reader := strings.NewReader(content)
+	filename := fmt.Sprintf("clipboard (%s).txt", time.Now().Format("2006-01-02 at 15.04.05"))
+
+	m.PerformUpload(reader, filename)
 }

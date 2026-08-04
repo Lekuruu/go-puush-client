3515890e9de55272d1b425324e0bb32780f027cf
Levi <contact@lekuru.xyz>
2026-08-04 22:43:23 +0200

    Move updater loop logic into own file

--- cmd/desktop/main.go
+++ cmd/desktop/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"math"
 	"os"
 	"os/exec"
 	"runtime"
@@ -15,7 +14,6 @@ import (
 	"github.com/Lekuruu/go-puush-client/internal/config"
 	"github.com/Lekuruu/go-puush-client/internal/desktop"
 	appipc "github.com/Lekuruu/go-puush-client/internal/ipc"
-	"github.com/Lekuruu/go-puush-client/internal/updater"
 	"github.com/Lekuruu/go-puush-client/pkg/puush"
 )
 
@@ -69,59 +67,8 @@ func run(arguments []string) error {
 
 	ui := desktop.NewUI(fyneApp, api, cfg)
 	ui.SetIPCServer(ipcResult.Server)
-
 	defer ui.OnShutdown()
-	go func() {
-		currentVersion, _ := updater.NewVersionFromString(AppVersion)
-
-		// Check if an old version is next to the current executable
-		wasUpdated := updater.Cleanup()
-		if wasUpdated {
-			log.Printf("puush was updated to version %s", currentVersion)
-			ui.ShowNotification("puush was updated!", "You are now running "+currentVersion.String())
-		}
-
-		// Periodically check for updates, if auto-updates are enabled inside the config
-		for {
-			// Start at 30 minutes and keep doubling time until we hit 6 hours max
-			// This is the same thing that the official puush client did
-			interval := math.Max(1000*60*30, math.Min(1000*60*360, float64(time.Hour)*2))
-			intervalDuration := time.Duration(interval) * time.Millisecond
-
-			if !cfg.Misc.UpdatesEnabled {
-				time.Sleep(intervalDuration)
-				return
-			}
-
-			candidate, err := updater.Check(currentVersion)
-			if err != nil {
-				log.Printf("Failed to check for updates: %v", err)
-				ui.ShowNotification("Update check failed!", "You may have to check for updates manually :(")
-				time.Sleep(intervalDuration)
-				continue
-			}
-			if candidate == nil {
-				log.Printf("No update available, current version: %s", currentVersion)
-				time.Sleep(intervalDuration)
-				continue
-			}
-
-			log.Printf("Update available: %s -> %s", currentVersion, candidate)
-			ui.ShowNotification("Downloading update...", "puush will automatically restart when done!")
-
-			err = updater.Perform(candidate)
-			if err != nil {
-				ui.ShowNotification("Update failed!", "You may have to install this update manually :(")
-				log.Printf("Failed to perform update: %v", err)
-				time.Sleep(intervalDuration)
-				continue
-			}
-
-			log.Printf("Update downloaded, restarting...")
-			restart()
-		}
-	}()
-
+	go updaterLoop(cfg, ui)
 	ui.Run()
 	return nil
 }
---
+++ cmd/desktop/updater.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"math"
+	"time"
+
+	"github.com/Lekuruu/go-puush-client/internal/config"
+	"github.com/Lekuruu/go-puush-client/internal/desktop"
+	"github.com/Lekuruu/go-puush-client/internal/updater"
+)
+
+func updaterLoop(cfg *config.Config, ui *desktop.UI) {
+	currentVersion, _ := updater.NewVersionFromString(AppVersion)
+
+	// Check if an old version is next to the current executable
+	wasUpdated := updater.Cleanup()
+	if wasUpdated {
+		log.Printf("puush was updated to version %s", currentVersion)
+		ui.ShowNotification("puush was updated!", "You are now running "+currentVersion.String())
+	}
+
+	// Periodically check for updates, if auto-updates are enabled inside the config
+	for {
+		// Start at 30 minutes and keep doubling time until we hit 6 hours max
+		// This is the same thing that the official puush client did
+		interval := math.Max(1000*60*30, math.Min(1000*60*360, float64(time.Hour)*2))
+		intervalDuration := time.Duration(interval) * time.Millisecond
+
+		if !cfg.Misc.UpdatesEnabled {
+			time.Sleep(intervalDuration)
+			return
+		}
+
+		candidate, err := updater.Check(currentVersion)
+		if err != nil {
+			log.Printf("Failed to check for updates: %v", err)
+			ui.ShowNotification("Update check failed!", "You may have to check for updates manually :(")
+			time.Sleep(intervalDuration)
+			continue
+		}
+		if candidate == nil {
+			log.Printf("No update available, current version: %s", currentVersion)
+			time.Sleep(intervalDuration)
+			continue
+		}
+
+		log.Printf("Update available: %s -> %s", currentVersion, candidate)
+		ui.ShowNotification("Downloading update...", "puush will automatically restart when done!")
+
+		err = updater.Perform(candidate)
+		if err != nil {
+			ui.ShowNotification("Update failed!", "You may have to install this update manually :(")
+			log.Printf("Failed to perform update: %v", err)
+			time.Sleep(intervalDuration)
+			continue
+		}
+
+		log.Printf("Update downloaded, restarting...")
+		restart()
+	}
+}

8d68825bba52fa185db487ef3610c9eb76010907
Levi <contact@lekuru.xyz>
2026-08-04 19:54:13 +0200

    Add linux helpers for dolphin, nautilus & nemo

---
+++ internal/contextmenu/linux_nemo.go
@@ -0,0 +1,23 @@
+//go:build linux
+
+package contextmenu
+
+import (
+	"path/filepath"
+)
+
+func nemoPath(context linuxContext) string {
+	return filepath.Join(context.dataHome, "nemo", "actions", "puush-upload.nemo_action")
+}
+
+func enableNemo(context linuxContext, executable string) error {
+	content, err := renderContextMenuTemplate("linux-nemo.action.tmpl", newContextMenuTemplateData(executable))
+	if err != nil {
+		return err
+	}
+	return writeOwnedFile(nemoPath(context), content, 0644)
+}
+
+func disableNemo(context linuxContext) error {
+	return removeOwnedFile(nemoPath(context))
+}

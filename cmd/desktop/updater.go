fb1eb5abc3b08bc1dabbc63e6c88c15ea47396fa
Levi <contact@lekuru.xyz>
2026-08-04 23:02:53 +0200

    Add todo for changelog page button

--- cmd/desktop/updater.go
+++ cmd/desktop/updater.go
@@ -17,7 +17,8 @@ func updaterLoop(cfg *config.Config, ui *desktop.UI) {
 	wasUpdated := updater.Cleanup()
 	if wasUpdated {
 		log.Printf("puush was updated to version %s", currentVersion)
-		ui.ShowNotification("puush was updated!", "You are now running "+currentVersion.String())
+		ui.ShowNotification("puush was updated!", "You are now running "+currentVersion.String()+".")
+		// TODO: Add button for opening changelog page on github
 	}
 
 	// Periodically check for updates, if auto-updates are enabled inside the config

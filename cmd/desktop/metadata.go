c790d55f42b94c6d0b1f3a3509f0834a7e2f7646
Levi <contact@lekuru.xyz>
2026-08-05 13:33:54 +0200

    Add compile-time git metadata to app

--- cmd/desktop/metadata.go
+++ cmd/desktop/metadata.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"github.com/Lekuruu/go-puush-client/assets"
@@ -10,22 +12,29 @@ const (
 	AppName     = "puush"
 	AppID       = "me.puush.client"
 	AppVersion  = "1.0.0"
-	AppBuild    = 100
 	AppIconName = "puush.png"
 )
 
+// Set by the build system through -ldflags -X
+var (
+	AppBuild  = "0"   // Number of current commits
+	AppCommit = "dev" // Current commit sha
+)
+
 func init() {
+	buildNumber, _ := strconv.Atoi(AppBuild)
+
 	app.SetMetadata(fyne.AppMetadata{
 		ID:      AppID,
 		Name:    AppName,
 		Version: AppVersion,
-		Build:   AppBuild,
+		Build:   buildNumber,
 		Icon: &fyne.StaticResource{
 			StaticName:    AppIconName,
 			StaticContent: assets.PuushIconData,
 		},
 		Release:    true,
-		Custom:     map[string]string{},
+		Custom:     map[string]string{"commit": AppCommit},
 		Migrations: map[string]bool{"fyneDo": true},
 	})
 }
